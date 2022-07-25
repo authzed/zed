3ed8dc6f93c886717c68a14e3e305a214fc126ca
Joseph Schorr <josephschorr@users.noreply.github.com>
2022-07-25 17:14:11 -0400

    Relax automatic prefix handling to allow multiple prefixes
    This was originally made strict before SpiceDB allowed multiple prefixes; now it is too strict
    
    Fixes #131

---
+++ cmd/zed/schema_test.go
@@ -0,0 +1,107 @@
+package main
+
+import (
+	"testing"
+
+	"github.com/stretchr/testify/require"
+)
+
+func TestDeterminePrefixForSchema(t *testing.T) {
+	tests := []struct {
+		name            string
+		existingSchema  string
+		specifiedPrefix string
+		expectedPrefix  string
+	}{
+		{
+			"empty schema",
+			"",
+			"",
+			"",
+		},
+		{
+			"no prefix, none specified",
+			`definition user {}`,
+			"",
+			"",
+		},
+		{
+			"no prefix, one specified",
+			`definition user {}`,
+			"test",
+			"test",
+		},
+		{
+			"prefix found",
+			`definition test/user {}`,
+			"",
+			"test",
+		},
+		{
+			"multiple prefixes found",
+			`definition test/user {}
+			
+			definition something/resource {}`,
+			"",
+			"",
+		},
+		{
+			"multiple prefixes found, one specified",
+			`definition test/user {}
+			
+			definition something/resource {}`,
+			"foobar",
+			"foobar",
+		},
+	}
+
+	for _, test := range tests {
+		t.Run(test.name, func(t *testing.T) {
+			found, err := determinePrefixForSchema(test.specifiedPrefix, nil, &test.existingSchema)
+			require.NoError(t, err)
+			require.Equal(t, test.expectedPrefix, found)
+		})
+	}
+}
+
+func TestRewriteSchema(t *testing.T) {
+	tests := []struct {
+		name             string
+		existingSchema   string
+		definitionPrefix string
+		expectedSchema   string
+	}{
+		{
+			"empty schema",
+			"",
+			"",
+			"",
+		},
+		{
+			"empty prefix schema",
+			"definition user {}",
+			"",
+			"definition user {}",
+		},
+		{
+			"empty prefix schema with specified",
+			"definition user {}",
+			"test",
+			"definition test/user {}",
+		},
+		{
+			"prefixed schema with specified",
+			"definition foo/user {}",
+			"test",
+			"definition foo/user {}",
+		},
+	}
+
+	for _, test := range tests {
+		t.Run(test.name, func(t *testing.T) {
+			found, err := rewriteSchema(test.existingSchema, test.definitionPrefix)
+			require.NoError(t, err)
+			require.Equal(t, test.expectedSchema, found)
+		})
+	}
+}
