e5c14c48ca74f0af8c559f7fa914f638f547e9cf
Tanner Stirrat <tstirrat@gmail.com>
2025-10-09 07:56:34 -0600

    breaking: Clean up deprecated functionality for v1

--- internal/cmd/preview.go
+++ internal/cmd/preview.go
@@ -4,19 +4,11 @@ import (
 	"github.com/spf13/cobra"
 )
 
-func registerPreviewCmd(rootCmd *cobra.Command, schemaCompileCmd *cobra.Command) {
+func registerPreviewCmd(rootCmd *cobra.Command) {
 	previewCmd := &cobra.Command{
 		Use:   "preview <subcommand>",
 		Short: "Experimental commands that have been made available for preview",
 	}
 
-	schemaCmd := &cobra.Command{
-		Use:        "schema <subcommand>",
-		Short:      "Manage schema for a permissions system",
-		Deprecated: "please use `zed schema compile`",
-	}
-
 	rootCmd.AddCommand(previewCmd)
-	previewCmd.AddCommand(schemaCmd)
-	schemaCmd.AddCommand(schemaCompileCmd)
 }
